81fa52d51ae1c96ea55ae747f3af98d3d64a1471
jealbertlopez08-diseño <jealbert.lopez08@gmail.com>
2026-07-14 19:38:47 -0600

    Cambios subidos  martes 14 de julio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.0.6"],
+  allowedDevOrigins: ["192.168.0.5"],
   /*allowedDevOrigins: ["192.168.0.0/16"],*/
 };
 
